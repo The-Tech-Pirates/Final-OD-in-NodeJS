f20fa71e245f3e8bb7d75e8e4012242b1df5ea53
Kanav Chauhan <kanavchauhan001@gmail.com>
2023-03-03 14:43:10 +0530

    local host to the moon

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const con = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Pass@SRM2023',
+  password: '',
   database: 'srmod',
   waitForConnections: true,
   connectionLimit: 10,
